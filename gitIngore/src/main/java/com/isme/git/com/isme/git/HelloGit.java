5d64dfc0a306c5e286032ff60c79a5b5542b61b8
isme <303182223@qq.com>
2021-04-14 20:54:14 +0800

    git commit

---
+++ gitIngore/src/main/java/com/isme/git/com/isme/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.isme.git.com.isme.git;
+
+/**
+ * @author isme
+ * @create 2021-04-14 20:50
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
